b8f7c77803ce8b5ed663528973ef0945a6011093
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-08-04 16:57:23 -0300

    Criação do total de conexões

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsControllers';
 
 const routes = express.Router();
 const classesController = new ClassesController();
+const connectionController = new ConnectionController();
 
 routes.post('/classes', classesController.create);
 routes.get('/classes', classesController.index);
+
+routes.get('/connections', connectionController.index);
+routes.post('/connections', connectionController.create);
 export default routes;
